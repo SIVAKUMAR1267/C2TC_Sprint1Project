25541f242cff52d56f0c02f10ef45b8fe6eecf67
sivakumar R <152311681+SIVAKUMAR1267@users.noreply.github.com>
2026-01-10 16:04:25 +0530

    adding shop owner service

--- MallAdminService/src/main/java/malladminservice/MallAdminRepository.java
+++
@@ -1,9 +0,0 @@
-package malladminservice;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;  
-
-@Repository
-public interface MallAdminRepository extends JpaRepository<MallAdmin, Integer>{
-
-}
-
